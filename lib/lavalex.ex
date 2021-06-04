d23df16006547a47ffbbaca52dca8c1dcb852d7f
Alex Hincu <teeseal@sorta.moe>
2021-06-04 18:14:21 +0300

    Initial commit

---
+++ lib/lavalex.ex
@@ -0,0 +1,18 @@
+defmodule Lavalex do
+  @moduledoc """
+  Documentation for `Lavalex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lavalex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lavalex_test.exs
@@ -0,0 +1,8 @@
+defmodule LavalexTest do
+  use ExUnit.Case
+  doctest Lavalex
+
+  test "greets the world" do
+    assert Lavalex.hello() == :world
+  end
+end
