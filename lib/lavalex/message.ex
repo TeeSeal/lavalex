96951898d9d7856f05b717b33b5a9195582b5b10
Alex Hincu <teeseal@sorta.moe>
2021-06-06 01:55:57 +0300

    fix(Lavalex.Message): stringify guild ID upon serialization

--- lib/lavalex/message.ex
+++ lib/lavalex/message.ex
@@ -1,4 +1,10 @@
 defmodule Lavalex.Message do
+  def serialize(%{guild_id: guild_id} = map) when is_integer(guild_id) do
+    map
+    |> Map.put(:guild_id, Integer.to_string(guild_id))
+    |> serialize()
+  end
+
   def serialize(struct) when is_struct(struct) do
     Map.from_struct(struct) |> serialize()
   end
