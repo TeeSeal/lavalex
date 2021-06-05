7a790dc672d6646fc93c638a893c9fc40f45c043
Alex Hincu <teeseal@sorta.moe>
2021-06-05 03:02:50 +0300

    feat: implement track loading/decoding API

--- lib/lavalex/http.ex
+++ lib/lavalex/http.ex
@@ -1,22 +1,30 @@
 defmodule Lavalex.HTTP do
   use HTTPoison.Base
 
+  def process_request_url(path), do: base_url() <> path
+
+  def process_request_headers(headers) do
+    headers ++
+      [
+        Authorization: Application.get_env(:lavalex, :password),
+        "Content-Type": "application/json"
+      ]
+  end
+
+  def process_request_body(body) when is_list(body) or is_map(body) do
+    Poison.encode!(body)
+  end
+
+  def process_request_body(body), do: body
+
   def process_response_body(body) do
     Poison.decode!(body)
-  end
-
-  def process_request_headers(_headers) do
-    ["Authorization": Application.get_env(:lavalex, :password)]
-  end
-
-  def process_request_url(path) do
-    base_url() <> path
   end
 
   def base_url do
     %URI{
       scheme: "http",
-      host: Application.get_env(:lavalex, :host),
+      host: Application.get_env(:lavalex, :host)
     }
     |> URI.to_string()
   end
---
+++ lib/lavalex/track.ex
@@ -0,0 +1,50 @@
+defmodule Lavalex.Track do
+  alias Lavalex.Track
+
+  defstruct [
+    :track,
+    :identifier,
+    :is_seekable,
+    :author,
+    :length,
+    :is_stream,
+    :position,
+    :title,
+    :uri
+  ]
+
+  def parse(%{ "track" => track, "info" => info}), do: parse(track, info)
+  def parse(track, info) do
+    %Track{
+      track: track,
+      identifier: info["identifier"],
+      is_seekable: info["isSeekable"],
+      author: info["author"],
+      length: info["length"],
+      is_stream: info["isStream"],
+      position: info["position"],
+      title: info["title"],
+      uri: info["uri"]
+    }
+  end
+
+  def load(identifier) do
+    Lavalex.HTTP.get!("/loadtracks", [], params: [identifier: identifier])
+    |> Map.get(:body)
+    |> Lavalex.LoadResponse.parse()
+  end
+
+  def decode(track) when is_list(track) do
+    Lavalex.HTTP.post!("/decodetracks", track)
+    |> Map.get(:body)
+    |> Enum.map(&Track.parse/1)
+  end
+
+  def decode(track) do
+    info =
+      Lavalex.HTTP.get!("/decodetrack", [], params: [track: track])
+      |> Map.get(:body)
+
+    Track.parse(track, info)
+  end
+end
